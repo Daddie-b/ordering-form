10b80836e608824f6ea538c033f0db0af643edca
Daddie-b <billwahomebill@gmail.com>
2024-05-25 15:42:36 +0300

    landing page edit

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,11 +1,13 @@
 // LandingPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AdminDashboard from './AdminDashBoard';
+import './LandingPage.css'; 
 
 function LandingPage() {
   return (
     <div>
-      <h1>Welcome to the Landing Page!</h1>
+      <h1>Welcome to the MugoMarbles </h1>
       <p>Click on the links below to navigate:</p>
       <ul>
         <li><Link to="/order">Order Form</Link></li>
