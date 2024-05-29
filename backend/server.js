e77844b7f7fcb55050a36a687142b2c9627b457d
Daddie-b <billwahomebill@gmail.com>
2024-05-29 16:41:08 +0300

    updates

--- backend/server.js
+++ backend/server.js
@@ -115,6 +115,6 @@ app.patch('/api/orders/:orderId', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
