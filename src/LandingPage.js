04dc625d3dca452b471e2d5559e9595ca261ed28
Daddie-b <billwahomebill@gmail.com>
2024-06-12 18:22:23 +0300

    css updates

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,25 @@
-// LandingPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import AdminDashboard from './AdminDashBoard';
-import './LandingPage.css'; 
+import './LandingPage.css';
 
 function LandingPage() {
   return (
-    <div>
-      <h1>Welcome to the MugoMarbles </h1>
-      <p>Click on the links below to navigate:</p>
-      <ul>
-        <li><Link to="/order">Order Form</Link></li>
-        <li><Link to="/admin">Admin Dashboard</Link></li>
-      </ul>
+    <div className="landing-page">
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/">Contact</Link></li>
+        </ul>
+      </nav>
+      <div className="content">
+        <h1>Welcome to MugoMarbles</h1>
+        <p>Click on the links below to navigate:</p>
+        <ul>
+          <li><Link to="/order">Order Form</Link></li>
+          <li><Link to="/admin">Admin Dashboard</Link></li>
+        </ul>
+      </div>
     </div>
   );
 }
